b26f699c8d36c53a6a26a40eb1c2b44b96f7d440
Michael Nitschinger <michael@nitschinger.at>
2020-04-03 20:38:45 +0200

    disable git2

--- crates/nu-cli/src/git.rs
+++ crates/nu-cli/src/git.rs
@@ -1,10 +1,11 @@
 #![cfg(not(feature = "starship-prompt"))]
 
-use git2::{Repository, RepositoryOpenFlags};
-use std::ffi::OsString;
+//use git2::{Repository, RepositoryOpenFlags};
+//use std::ffi::OsString;
 
 pub fn current_branch() -> Option<String> {
-    let v: Vec<OsString> = vec![];
+    None
+    /*let v: Vec<OsString> = vec![];
     match Repository::open_ext(".", RepositoryOpenFlags::empty(), v) {
         Ok(repo) => {
             let r = repo.head();
@@ -17,5 +18,5 @@ pub fn current_branch() -> Option<String> {
             }
         }
         _ => None,
-    }
+    }*/
 }
